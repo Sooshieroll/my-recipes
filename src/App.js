bde2a5b02cd05d23def4604f668770364b8a1abe
Sooshieroll <lucyy.wang95@gmail.com>
2023-02-27 18:04:51 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -8,26 +8,29 @@ import Alert from './components/Alert';
 
 
 
+
 function App() {
 
   const[search, setSearch] = useState('');
   const[recipes, setRecipes] = useState([]);
   const [alert, setAlert] = useState('');
 
-  const url =`http://api.edamam.com/search?q=${search}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`
+  const url =`https://api.edamam.com/search?q=${search}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`;
+
 
   const getData = async() => {
       if(search !== '') {
         const result = await Axios.get(url);
         if(!result.data.more) {
-          return setAlert('No results found');
+        
+          return setAlert('No results found 😔');
         }
         setRecipes(result.data.hits)
         console.log(result);
         setAlert('');
         setSearch('');
       } else {
-        setAlert('Please fill out the form')
+        setAlert('Please fill out the form 😉')
       }
   };
 
@@ -45,10 +48,10 @@ function App() {
     <div className="finder">
       <h1>Recipe Finder</h1>
       <form className = 'search-form' onSubmit={onSubmit}>
-        {alert !== '' && <Alert alert = {alert}/>}
         <input className='search' type='text' placeholder ='Search Food' autoComplete='off' onChange={onChange} value={search}/>
         <input className='submit-button' type='submit' value='search'/>
       </form>
+      {alert !== '' && <Alert alert = {alert}/>}
       <div className='recipes'>
         {recipes !== [] && recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe}/>)}
       </div>
